d34d8efa031987211a5d4fb1350bded4e9a3ae54
Superfl0 <florian.rumpf14@gmail.com>
2021-02-07 18:19:44 +0100

    Finished plots

--- incidence_mortality_plot.R
+++ incidence_mortality_plot.R
@@ -1,16 +1,52 @@
 library(tidyverse)
+library(ggthemes)
 
+rm(list=ls())
 
 incidence <- read.table("Data/Inzidenz/Krebsdaten.csv", sep = ";", dec = ",", header = TRUE, na.strings = c("x"))
 mortality <- read.csv("Data/Mortalität/Krebsdaten.csv", sep = ";", dec = ",", header = TRUE, na.strings = c("x"))
 incidence$X85. <-  as.numeric(sub(",", ".", incidence$X85., fixed = TRUE))
-glimpse(incidence)
+summary(incidence)
 
 
 colnames(incidence)[1:3] <- c("year", "sex", "cancer")
-incidence_tidy <- incidence %>% gather(key = "age_group", value = "incidence", -year, -sex, -cancer) %>% group_by(year, sex, cancer) %>% summarise(incidence_total = sum(incidence, na.rm = TRUE))
+incidence_tidy <- incidence %>% gather(key = "age_group", value = "incidence", -year, -sex, -cancer) %>% group_by(year, sex, cancer) %>% summarise(incidence_total = sum(incidence, na.rm = TRUE)) %>% ungroup()
 
 colnames(mortality)[1:3] <- c("year", "sex", "cancer")
-mortality_tidy <- mortality %>% gather(key = "age_group", value = "mortality", -year, -sex, -cancer) %>% group_by(year, sex, cancer) %>% summarise(mortality_total = sum(mortality, na.rm = TRUE))
+mortality_tidy <- mortality %>% gather(key = "age_group", value = "mortality", -year, -sex, -cancer) %>% group_by(year, sex, cancer) %>% summarise(mortality_total = sum(mortality, na.rm = TRUE)) %>% ungroup()
 
+incidence_plot <- incidence_tidy %>% filter(year == 2016) %>% arrange(desc(incidence_total))
+incidence_plot[which(incidence_plot$sex != "weiblich"),"incidence_total"] <- incidence_plot[which(incidence_plot$sex != "weiblich"),"incidence_total"] * (-1)
+incidence_plot$incidence_total_abs <- abs(incidence_plot$incidence_total)
+
+incidence_plot <- incidence_plot %>% filter(str_detect(incidence_plot$cancer, c("Prostata|Darm|Lunge|Brust|Melanom|Harnblase|Bauchspeichel|Non-Hodgkin-Lymphom|Magen|Leuk")))
+incidence_plot$cancer
+
+gg_incidence <- ggplot(data = incidence_plot, aes(fill = sex, x = incidence_total, y = reorder(cancer, incidence_total_abs))) +
+  geom_bar(stat = "identity")+
+  scale_x_continuous(name = "Annual number of new cancer cases\nper 100,000 individuals in Germany", breaks = c(-2000, -1000, 0, 1000, 2000), labels = c(2000, 1000, 0, 1000, 2000)) +
+  scale_y_discrete(name = "", labels = rev(c("Prostate", "Colon", "Lung", "Breast", "Melanoma", "Bladder", "Pankreas", "Non-Hodgkin\nLymphoma", "Gastric", "Leukemia"))) +
+  scale_fill_discrete(name = "", labels = c("Male", "Female")) +
+  theme_minimal() +
+  theme(axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)), legend.position = "top")
+gg_incidence
+ggsave(gg_incidence, filename = "gg_incidence.png", device = "png", width = 12, height = 15, units = "cm")
+
+
+mortality_plot <- mortality_tidy %>% filter(year == 2016) %>% arrange(desc(mortality_total))
+mortality_plot[which(mortality_plot$sex != "weiblich"),"mortality_total"] <- mortality_plot[which(mortality_plot$sex != "weiblich"),"mortality_total"] * (-1)
+mortality_plot$mortality_total_abs <- abs(mortality_plot$mortality_total)
+
+mortality_plot <- mortality_plot %>% filter(str_detect(mortality_plot$cancer, c("Prostata|Darm|Lunge|Brust|Harnblase|Bauchspeichel|Non-Hodgkin-Lymphom|Magen|Leuk|Leber")))
+
+gg_mortality <- ggplot(data = mortality_plot, aes(fill = sex, x = mortality_total, y = reorder(cancer, mortality_total_abs))) +
+  geom_bar(stat = "identity")+
+  scale_x_continuous(name = "Annual number of cancer deaths\nper 100,000 individuals in Germany", breaks = c(-1000, -500, 0, 500, 1000), labels = c(1000, 500, 0, 500, 1000)) +
+  scale_y_discrete(name = "", labels = rev(c("Lung", "Colon", "Prostate", "Pankreas", "Breast", "Gastric", "Leukemia", "Liver", "Bladder", "Non-Hodgkin\nLymphoma"))) +
+  scale_fill_discrete(name = "", labels = c("Male", "Female")) +
+  theme_minimal() +
+  theme(axis.title.x = element_text(margin = margin(t = 10, r = 0, b = 0, l = 0)), legend.position = "top")
+gg_mortality
+
+ggsave(gg_mortality, filename = "gg_mortality.png", device = "png", width = 12, height = 15, units = "cm")
 
---
+++ overall_mortality_plot.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(treemapify)
+
+IN <- read.csv("Data/23211-0004.csv", sep = ";", skip = 10, stringsAsFactors = TRUE, header = FALSE, na.strings = c("-", ""))#, colClasses = c("numeric", "numeric", "factor", "factor", rep("numeric", 23)))
+IN <-IN[-(81:84),]
+IN <- IN[,-1]
+
+IN$parent <- c("total", 
+               rep("Infektionen", 5), 
+               rep("Neoplasien", 25), 
+               "Andere", 
+               "Endokrin", 
+               "Endokrin", 
+               rep("Psychiatrisch", 3), 
+               "Neurologisch", 
+               "Infektionen", 
+               rep("Kardiovaskulär", 9), 
+               rep("Respiratorisch", 5), 
+               rep("Gastroenterologisch", 2), 
+               rep("Leber", 2),
+               "Andere",
+               rep("Muskuloskelletal", 2),
+               rep("Urogenital", 2),
+               "Schwangerschaft",
+               rep("Kongenital", 4),
+               "Andere",
+               "Schwangerschaft",
+               "Andere",
+               rep("External", 10)
+               )
+
+
+IN <- IN %>% gather(key = "delete", value = "deaths", -V2, -parent) %>% group_by(V2) %>% summarise(deaths_total = sum(deaths, na.rm = TRUE), parent = first(parent)) %>% ungroup()
+
+IN <- IN %>% filter(!str_detect(V2, "Insgesamt|Insgesamt|Neubildungen|Krankheiten des Kreislaufsystems|Krankheiten des Atmungssystems|Fehlbildungen,Deformitäten|Äußere Ursachen von Morbidität und Mortalität"))
+
+
+treemap <- ggplot(data = IN, aes(area = deaths_total, fill = parent, subgroup = parent, label = V2)) + 
+  geom_treemap(size = 1) +
+  #geom_treemap_subgroup_border(colour = "black", alpha = 0.5, size = 2)+
+  geom_treemap_text(colour = "white", place = "topleft", reflow = T) +
+  geom_treemap_subgroup_text(place = "bottomleft", grow = T, alpha = 0.5, colour =
+                               "black", fontface = "italic", min.size = 0, padding.x = grid::unit(5,"mm"), padding.y = grid::unit(5,"mm")) +
+  guides(fill = FALSE)
+treemap
+ggsave(treemap, filename = "treemap.png", device = "png", width = 20, height = 12, units = "cm")
